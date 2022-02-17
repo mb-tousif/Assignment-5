73b73ccfc4f991225c1a1e7a42718a5a1ecb58bf
[mb-tousif] <[meta.block.tousif@gmail.com]>
2022-02-17 10:13:43 +0600

    JS code for Saving Section

--- app.js
+++ app.js
@@ -11,15 +11,34 @@ document.getElementById("calculationButton").addEventListener("click", function(
     let balanceAfterExpense = document.getElementById("balance");
     balanceAfterExpense.innerText = balanceCalculation(totalIncome,expenditure);
     // -----Clear Input Field-----
-    incomeField.value = "";
     foodField.value = "";
     rentField.value = "";
     clothesField.value = "";
-    // income.value = "";
 })
 // --------------Saving Section-----------------
 document.getElementById("savingButton").addEventListener("click", function(){
-    
+  let income = document.getElementById("incomeField").value;
+  const totalIncome = parseInt(income);
+  const savingAmt = document.getElementById("savingField").value;
+  const savingMoney = (parseInt(savingAmt) / 100) * totalIncome;
+  // console.log(savingMoney);
+  let saving = document.getElementById("totalSaving");
+  saving.innerText = savingMoney;
+  let food = document.getElementById("foodField").value;
+  let rent = document.getElementById("rentField").value;
+  let clothes = document.getElementById("clothesField").value;
+  let expenditure = parseInt(food) + parseInt(rent) + parseInt(clothes);
+  const totalExpenditure = parseInt(saving) + parseInt(expenditure);
+  console.log(totalExpenditure);
+//    let balance = document.getElementById("remainingBalance");
+//    balance.innerText = balanceCalculation(totalIncome, totalExpenditure);
+
+  // -----Clear Input Field-----
+  incomeField.value = "";
+  foodField.value = "";
+  rentField.value = "";
+  clothesField.value = "";
+  savingField.value = "";
 })
 //-------Generel Function--------
 function balanceCalculation(a,b){
